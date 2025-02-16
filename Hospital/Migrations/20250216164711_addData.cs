e90f0dac36ee9e2602cc546f51150d3af3828e22
Abdelrahmanmoussan <abdelrahmanmoussan339@gmail.com>
2025-02-16 19:35:49 +0200

    Add Patient Table

---
+++ Hospital/Migrations/20250216164711_addData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital.Migrations
+{
+    /// <inheritdoc />
+    public partial class addData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_doctors",
+                table: "doctors");
+
+            migrationBuilder.RenameTable(
+                name: "doctors",
+                newName: "Doctors");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Doctors",
+                table: "Doctors",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "PatientAppointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppointmentDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    AppointmentTime = table.Column<TimeOnly>(type: "time", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    DoctorName = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientAppointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatientAppointments_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientAppointments_DoctorId",
+                table: "PatientAppointments",
+                column: "DoctorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PatientAppointments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Doctors",
+                table: "Doctors");
+
+            migrationBuilder.RenameTable(
+                name: "Doctors",
+                newName: "doctors");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_doctors",
+                table: "doctors",
+                column: "Id");
+        }
+    }
+}
---
+++ Hospital/Migrations/20250216172444_EditStringDoctorName.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditStringDoctorName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DoctorName",
+                table: "PatientAppointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DoctorName",
+                table: "PatientAppointments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
